0d65af9b713b96b6ae9cb20f391910b4de51acea
goryanych <kolohryvyi@gmail.com>
2021-02-28 02:02:16 +0200

    fix: update solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,14 +6,14 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
   if (
     !Number(sampleActivity) ||
-    !(typeof sampleActivity === 'string')
-    || (sampleActivity < 0 || sampleActivity > 15)) {
+    !(typeof sampleActivity === 'string') ||
+    (sampleActivity <= 0 || sampleActivity > 15)) {
     return false;
   }
 
-  const calculatingTime = Math.ceil(MODERN_ACTIVITY / sampleActivity);
-  const reactionRate = Math.ceil(0.693 / HALF_LIFE_PERIOD);
-  const result = calculatingTime / reactionRate;
+  const calculatingTime = Math.log(MODERN_ACTIVITY / sampleActivity);
+  const reactionRate = 0.693 / HALF_LIFE_PERIOD;
+  const result = Math.ceil(calculatingTime / reactionRate);
 
   return result;
 };
